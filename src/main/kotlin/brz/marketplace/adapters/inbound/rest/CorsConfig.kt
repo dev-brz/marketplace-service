23f3426e7c2f05fc29f29a6d6383115eb4429f87
dev-brz <160502200+dev-brz@users.noreply.github.com>
2025-12-07 18:28:12 +0100

    fix: fix openapi response media type & add cors config

---
+++ src/main/kotlin/brz/marketplace/adapters/inbound/rest/CorsConfig.kt
@@ -0,0 +1,27 @@
+package brz.marketplace.adapters.inbound.rest
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+private class CorsConfig(
+    @Value($$"${ui.url}") private val uiUrl: String
+) {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(uiUrl)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+                    .maxAge(3600)
+            }
+        }
+    }
+}
